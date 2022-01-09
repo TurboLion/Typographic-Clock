a1eebeaefc260dbace1833fd8c797297b98304df
TurboLion <TurboLion@users.noreply.github.com>
2022-01-09 13:01:32 +0200

    Format code

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const settings = {
   fps: 0,
   isLightTheme: false,
   highlightColor: "#f57c00", // Orange 700
-  scale:100
+  scale: 100,
 };
 
 // Matrix for numbers in a 3x5 grid
@@ -81,8 +81,9 @@ window.wallpaperPropertyListener = {
       setHighlightColor(settings.highlightColor);
     }
 
-    if(properties.scale){
-      document.body.style.transform = 'scale(' + properties.scale.value/100 + ')';
+    if (properties.scale) {
+      document.body.style.transform =
+        "scale(" + properties.scale.value / 100 + ")";
     }
   },
 };
